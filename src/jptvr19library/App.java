b5a4713f3a52d4c3b7d63490b2015035223a6937
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2020-10-01 09:42:26 +0300

    Создан интерфейс управления программой

--- src/jptvr19library/App.java
+++ src/jptvr19library/App.java
@@ -5,12 +5,54 @@
  */
 package jptvr19library;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Melnikov
  */
 class App {
     public void run(){
+        boolean repeat = true;
         System.out.println("--- Библиотека ---");
+        do{
+            System.out.println("Задачи: ");
+            System.out.println("0. Выйти из программы");
+            System.out.println("1. Добавить новую книгу");
+            System.out.println("2. Список книг");
+            System.out.println("3. Зарегистрировать читателя");
+            System.out.println("4. Список читателей");
+            System.out.println("5. Выдать книгу читателю");
+            System.out.println("6. Вернуть книгу в библиотеку");
+            System.out.print("Выберите задачу: ");
+            Scanner scanner = new Scanner(System.in);
+            String task = scanner.nextLine();
+            switch (task) {
+                case "0":
+                    System.out.println("--- конец программы ---");
+                    repeat = false;
+                    break;
+                case "1":
+                    System.out.println("--- Добавить новую книгу ---");
+                    break;
+                case "2":
+                    System.out.println("--- Список книг ---");
+                    break;
+                case "3":
+                    System.out.println("--- Зарегистрировать читателя ---");
+                    break;
+                case "4":
+                    System.out.println("--- Список читателей ---");
+                    break;
+                case "5":
+                    System.out.println("--- Выдать книгу читателю ---");
+                    break;
+                case "6":
+                    System.out.println("--- Вернуть книгу в библиотеку ---");
+                    break;
+                default:
+                    System.out.println("Нет такой задачи.");
+            }
+        }while(repeat);
     }
 }
