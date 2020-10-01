d42bc7bd4df77d4fa97f8b61288ddbad87f0be1f
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2020-10-01 09:16:19 +0300

    Заготовка проекта

---
+++ src/jptvr19library/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19library;
+
+/**
+ *
+ * @author Melnikov
+ */
+class App {
+    public void run(){
+        System.out.println("--- Библиотека ---");
+    }
+}
